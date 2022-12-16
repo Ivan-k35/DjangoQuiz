c9ab918d9f056c076b2fab5d6ad2da1538ac0208
Ivan Koshkin <ivan.koshkin35@gmail.com>
2022-12-16 20:23:58 +0300

    Update templates and views.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.CategoryListView.as_view(), name='home'),
-    path('quiz/', views.QuizListView.as_view(), name='quiz_list'),
+    path('quiz/', views.QuizListView.as_view(), name='quiz-list'),
     path('quiz/<pk>/', views.quiz_view, name='quiz'),
+    path('quiz/<pk>/data', views.quiz_date_view, name='quiz-data')
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Category, Quiz
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 
 class CategoryListView(ListView):
     model = Category
     template_name = 'main/index.html'
     context_object_name = 'categories'
+    allow_empty = False
 
     def get(self, *args, **kwargs):
         if self.request.GET.get('category') and self.request.GET.get('category') != 'Choose':
@@ -18,14 +21,26 @@ class QuizListView(ListView):
     model = Quiz
     template_name = 'main/quiz_list.html'
     context_object_name = 'quizes'
+    allow_empty = False
 
     def get_queryset(self):
-        return Quiz.objects.filter(category__category_name=self.request.GET.get('category')).first()
+        return Quiz.objects.filter(category__category_name=self.request.GET.get('category'))
 
 
 def quiz_view(request, pk):
-    quiz = Quiz.objects.get(pk=pk)
+    quiz = get_object_or_404(Quiz, pk=pk)
     context = {'obj': quiz}
     return render(request, 'main/quiz.html', context)
 
 
+def quiz_date_view(request, pk):
+    quiz = get_object_or_404(Quiz, pk=pk)
+    questions = []
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answers():
+            answers.append(a.text)
+        questions.append({str(q): answers})
+    return JsonResponse({
+        'data': questions
+    })
