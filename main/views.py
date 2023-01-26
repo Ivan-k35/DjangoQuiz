28098205a0b0f2bfc1b0c7d496d349792cf8876c
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-01-26 17:43:55 +0300

    Update views.py, login.html, register.html

--- main/views.py
+++ main/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, redirect
-from .models import Category, Quiz
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
+from django.urls import reverse_lazy
+from .models import Category, Quiz
 
 
 def index(request):
     return render(request, 'main/index.html', {'title': 'Главная страница'})
 
 
-def register(request):
-    return render(request, 'main/register.html', {'title': 'register'})
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = 'main/register.html'
+    success_url = reverse_lazy('main:login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация'
+        return context
 
 
 def login(request):
-    return render(request, 'main/login.html', {'title': 'login'})
+    return render(request, 'main/login.html', {'title': 'Войти'})
 
 
 class CategoryListView(ListView):
